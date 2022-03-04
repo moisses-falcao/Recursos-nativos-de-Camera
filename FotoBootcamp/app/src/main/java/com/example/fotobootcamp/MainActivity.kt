433cb476de46a0612c3014906370ec30c64d2965
Moissés Falcão <moissesvrf@gmail.com>
2022-03-04 17:45:27 -0300

    commit inicial

---
+++ FotoBootcamp/app/src/main/java/com/example/fotobootcamp/MainActivity.kt
@@ -0,0 +1,133 @@
+package com.example.fotobootcamp
+
+import android.Manifest
+import android.app.Activity
+import android.content.ContentValues
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import com.example.fotobootcamp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    var image_uri: Uri? = null
+
+    companion object {
+        private val PERMISSION_CODE_IMAGE_PICK = 1000
+        private val IMAGE_PICK_CODE = 1001
+
+        private val PERMISSION_CODE_CAMERA_CAPTURE = 2000
+        private val OPEN_CAMERA_CODE = 2001
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        var binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.pickButtom.setOnClickListener {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED
+                ) {
+                    val permission = kotlin.arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    requestPermissions(permission, PERMISSION_CODE_IMAGE_PICK)
+                } else {
+                    pickImageFromGalery()
+                }
+            } else {
+                pickImageFromGalery()
+            }
+
+        }
+
+        binding.openCameraButtom.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+                if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED
+                    ||
+                    checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                            == PackageManager.PERMISSION_DENIED) {
+
+                    val permission = arrayOf(
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    )
+
+                    requestPermissions(permission, PERMISSION_CODE_CAMERA_CAPTURE)
+
+                } else {
+                    openCamera()
+                }
+
+            } else {
+                openCamera()
+            }
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSION_CODE_IMAGE_PICK -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    pickImageFromGalery()
+                } else {
+                    Toast.makeText(this, "Permissão Negada", Toast.LENGTH_SHORT).show()
+                }
+            }
+            PERMISSION_CODE_CAMERA_CAPTURE -> {
+                if (grantResults.size > 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    openCamera()
+                } else {
+                    Toast.makeText(this, "Permissão Negada", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun openCamera() {
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "Nova foto")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "Imagem capturada pela camera")
+
+        image_uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
+
+        startActivityForResult(cameraIntent, OPEN_CAMERA_CODE)
+
+    }
+
+    private fun pickImageFromGalery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        var binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            binding.imageView.setImageURI(data?.data)
+        }
+        if (resultCode == Activity.RESULT_OK && requestCode == OPEN_CAMERA_CODE) {
+            binding.imageView.setImageURI(image_uri)
+        }
+
+    }
+
+}
